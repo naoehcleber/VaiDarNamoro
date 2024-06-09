a6b7a333ad021e8884629e131aabe1e8f50246b6
Anabia <ana.00000850038@unicap.br>
2024-06-09 17:58:09 -0300

    criacao arquivo verifica arquivos

--- src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VaiDarNamoroTerminal.java
+++ src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VaiDarNamoroTerminal.java
@@ -36,49 +36,7 @@ public class VaiDarNamoroTerminal {
         String respostaCorreta;
         Perguntas proximaPergunta;
 
-        try {
-            File file = new File("respostas_padrao.txt");
-            File arquivoUsuario = new File("Perfil_Usuario.txt");
-            if (arquivoUsuario.exists()) {
-                Scanner leitorArquivo = new Scanner(arquivoUsuario);
-                currentLineFile = 0;
-                while (leitorArquivo.hasNextLine()) {
-                    String respostasArquivo = leitorArquivo.nextLine();
-                    respostas.salvarRespostas(respostasArquivo, currentLineFile);
-                    currentLineFile++;
-                }
-                leitorArquivo.close();
-            } else if (file.exists()) {
-                Scanner leitorArquivo = new Scanner(file);
-                currentLineFile = 0;
-
-                while(leitorArquivo.hasNextLine()){
-                    
-                    //1) salva as linhas do arquivo em uma variavel String
-
-                while (leitorArquivo.hasNextLine()) {
-
-                    String respostasArquivo = leitorArquivo.nextLine();
-                    respostas.salvarRespostas(respostasArquivo, currentLineFile);
-                    currentLineFile++;
-                }
-                leitorArquivo.close();
-                }
-            } else {
-                file.createNewFile();
-                try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
-                    for(int i = 0; i < perguntas.getTamanhoPerguntas(); i++){
-                        writer.write("A");
-                        writer.newLine();
-                    }
-                } catch (IOException e){
-                    System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
-                }
-                
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
 
         do {
             System.out.println("Insira seu nome");
