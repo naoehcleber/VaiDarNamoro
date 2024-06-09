4295a1c67e9937264bd0a982f0cbd884131eaa80
Anabia <ana.00000850038@unicap.br>
2024-06-09 18:58:38 -0300

    Ajustes arquvo verifica controle

--- src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VerificaArquivo.java
+++ src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VerificaArquivo.java
@@ -1,49 +1,52 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
+public class VerificaArquivo {
+    public static void main(String[] args) {
+        try {
+            VerificarResposta respostas = new VerificarResposta(); //   Suponho que exista uma classe Respostas que você esteja usando
+            QuestionarioController perguntas = new QuestionarioController();
+            int currentLineFile;
 
-
-public class VerificaArquivo{
-    try {
-        File file = new File("respostas_padrao.txt");
-        File arquivoUsuario = new File("Perfil_Usuario.txt");
-        if (arquivoUsuario.exists()) {
-            Scanner leitorArquivo = new Scanner(arquivoUsuario);
-            currentLineFile = 0;
-            while (leitorArquivo.hasNextLine()) {
-                String respostasArquivo = leitorArquivo.nextLine();
-                respostas.salvarRespostas(respostasArquivo, currentLineFile);
-                currentLineFile++;
-            }
-            leitorArquivo.close();
-        } else if (file.exists()) {
-            Scanner leitorArquivo = new Scanner(file);
-            currentLineFile = 0;
-
-            while(leitorArquivo.hasNextLine()){
-                
-                //1) salva as linhas do arquivo em uma variavel String
-
-            while (leitorArquivo.hasNextLine()) {
-
-                String respostasArquivo = leitorArquivo.nextLine();
-                respostas.salvarRespostas(respostasArquivo, currentLineFile);
-                currentLineFile++;
-            }
-            leitorArquivo.close();
-            }
-        } else {
-            file.createNewFile();
-            try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
-                for(int i = 0; i < perguntas.getTamanhoPerguntas(); i++){
-                    writer.write("A");
-                    writer.newLine();
+            File arquivoUsuario = new File("Perfil_Usuario.txt");
+            if (arquivoUsuario.exists()) {
+                Scanner leitorArquivo = new Scanner(arquivoUsuario);
+                currentLineFile = 0;
+                while (leitorArquivo.hasNextLine()) {
+                    String respostasArquivo = leitorArquivo.nextLine();
+                    respostas.salvarRespostas(respostasArquivo, currentLineFile);
+                    currentLineFile++;
                 }
-            } catch (IOException e){
-                System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+                leitorArquivo.close();
+            } else {
+                File file = new File("respostas_padrao.txt");
+                if (file.exists()) {
+                    Scanner leitorArquivo = new Scanner(file);
+                    currentLineFile = 0;
+
+                    while (leitorArquivo.hasNextLine()) {
+                        String respostasArquivo = leitorArquivo.nextLine();
+                        respostas.salvarRespostas(respostasArquivo, currentLineFile);
+                        currentLineFile++;
+                    }
+                    leitorArquivo.close();
+                } else {
+                    file.createNewFile();
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                        for (int i = 0; i < perguntas.getTamanhoPerguntas(); i++) {
+                            writer.write("A");
+                            writer.newLine();
+                        }
+                    } catch (IOException e) {
+                        System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+                    }
+                }
             }
-            
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    } catch (IOException e) {
-        e.printStackTrace();
     }
 }
