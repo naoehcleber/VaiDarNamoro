6eac575da5d5a971c2ae18f7f1dc888261811a75
Joao Victor <joao.vr.fernandes@gmail.com>
2024-06-10 20:42:36 -0300

    perguntas e respostas escritas

--- src/main/java/com/mycompany/vaidarnamoroterminal/Entity/Questao.java
+++ src/main/java/com/mycompany/vaidarnamoroterminal/Entity/Questao.java
@@ -16,11 +16,11 @@ public class Questao {
     public Questao(String fraseP) {
         this.FraseP = fraseP;
         respostas = new ArrayList<Resposta>();
-        respostas.add(new Resposta( "A -> Tal coisa", "B -> Tal coisa", "C -> Tal coisa"));
-        respostas.add(new Resposta( "A -> Tal coisa", "B -> Tal coisa", "C -> Tal coisa"));
-        respostas.add(new Resposta( "A -> Tal coisa", "B -> Tal coisa", "C -> Tal coisa"));
-        respostas.add(new Resposta( "A -> Tal coisa", "B -> Tal coisa", "C -> Tal coisa"));
-        respostas.add(new Resposta( "A -> Tal coisa", "B -> Tal coisa", "C -> Tal coisa"));
+        respostas.add(new Resposta( "A -> Praia", "B -> Restaurante 5 estrelas", "C -> Festa clandestina"));
+        respostas.add(new Resposta( "A -> Passear pelo shopping", "B -> Ler e ver filmes", "C -> Dormir"));
+        respostas.add(new Resposta( "A -> Livros e filmes", "B -> Um MP3 Player", "C -> Quatro mil e oitocentas bolas de gude"));
+        respostas.add(new Resposta( "A -> Forte e corajoso", "B -> Inteligente", "C -> Engracado"));
+        respostas.add(new Resposta( "A -> Mantenho minha escolha", "B -> Mudo minha escolha", "C -> Morro"));
 
         
     }
--- src/main/java/com/mycompany/vaidarnamoroterminal/Entity/Questionario.java
+++ src/main/java/com/mycompany/vaidarnamoroterminal/Entity/Questionario.java
@@ -19,11 +19,11 @@ public class Questionario {
         
         perguntas = new ArrayList<Questao>();
         
-        perguntas.add(new Questao("Pergunta 1"));
-        perguntas.add(new Questao("Pergunta 2"));
-        perguntas.add(new Questao("Pergunta 3"));
-        perguntas.add(new Questao("Pergunta 4"));
-        perguntas.add(new Questao("Pergunta 5"));
+        perguntas.add(new Questao("Aonde voce gostaria que fosse seu date ideal ?"));
+        perguntas.add(new Questao("O que voce gosta de fazer no tempo livre ?"));
+        perguntas.add(new Questao("O que voce levaria para uma ilha deserta ?"));
+        perguntas.add(new Questao("O seu parceiro ideal tem que ser alguem : "));
+        perguntas.add(new Questao("O problema de Monty Hall consiste em um progama de televisao aonde existem 3 portas. Por tras de uma das tres portas existe um carro que sera seu se voce acertar a porta a qual ele esta detras. Voce escolhe uma porta, e o apresentador abre uma das outras duas, sabendo que o carro nao estava detras da porta aberta, o que voce faz ?"));
         
         
         
