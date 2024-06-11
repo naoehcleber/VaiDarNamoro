a6a1a3d78fc7bc4ad0bfbe6571db0a8bb4cb0a3f
Anabia <ana.00000850038@unicap.br>
2024-06-11 07:34:43 -0300

    reajustando a main

--- src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VaiDarNamoroTerminal.java
+++ src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VaiDarNamoroTerminal.java
@@ -22,28 +22,66 @@ public class VaiDarNamoroTerminal {
 
     public static void main(String[] args) throws EntradaInvalida {
         Scanner input = new Scanner(System.in);
-        int opcaoPerfil;
         
 
         QuestionarioController perguntas = new QuestionarioController();
         VerificarResposta respostas = new VerificarResposta();
         UsuarioController voce = new UsuarioController();
         Usuario usuario = new Usuario();
+        
         int IndexAtual;
         Questao proximaPergunta;
         String respostaJogo;
         String respostaCorreta;
-        //ListaPerguntas perguntas = new ListaPerguntas();
-        //Respostas respostas = new Respostas();
-        //Pessoa pessoa = new Pessoa();
-        //Usuario voce = new Usuario();
-
         int opcaoMenu;
         int currentLineFile;
-        
-        
+        int opcaoPerfil;
         String nomeUsuario;
-        
+       
+
+        try {
+            File file = new File("respostas_padrao.txt");
+            File arquivoUsuario = new File("Perfil_Usuario.txt");
+            if (arquivoUsuario.exists()) {
+                Scanner leitorArquivo = new Scanner(arquivoUsuario);
+                currentLineFile = 0;
+                while (leitorArquivo.hasNextLine()) {
+                    String respostasArquivo = leitorArquivo.nextLine();
+                    respostas.salvarRespostas(respostasArquivo, currentLineFile);
+                    currentLineFile++;
+                }
+                leitorArquivo.close();
+            } else if (file.exists()) {
+                Scanner leitorArquivo = new Scanner(file);
+                currentLineFile = 0;
+
+                while(leitorArquivo.hasNextLine()){
+                    
+                    //1) salva as linhas do arquivo em uma variavel String
+
+                while (leitorArquivo.hasNextLine()) {
+
+                    String respostasArquivo = leitorArquivo.nextLine();
+                    respostas.salvarRespostas(respostasArquivo, currentLineFile);
+                    currentLineFile++;
+                }
+                leitorArquivo.close();
+                }
+            } else {
+                file.createNewFile();
+                try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+                    for(int i = 0; i < perguntas.getTamanhoPerguntas(); i++){
+                        writer.write("A");
+                        writer.newLine();
+                    }
+                } catch (IOException e){
+                    System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
+                }
+                
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         do {
             System.out.println("Insira seu nome");
@@ -66,58 +104,82 @@ public class VaiDarNamoroTerminal {
             if (opcaoMenu != 1 && opcaoMenu != 2 && opcaoMenu != 0) {
                 throw new EntradaInvalida("Insira uma opcao valida por favor");
             }
+            perguntas.restartCounter();
 
             if (opcaoMenu == 1) {
-                perguntas.restartCounter();
-        System.out.println("Escolha qual perfil para ver a compatibilidade :");
-        System.out.println("1 - Padrão");
-        System.out.println("2 - Perfil do Usuario");
-        opcaoPerfil = input.nextInt();
-        if (opcaoPerfil == 1) {
-            respostas.lerRespostasArquivo("respostas_padrao.txt");
-        } else if (opcaoPerfil == 2) {
-            respostas.lerRespostasArquivo("Perfil_usuario.txt");
-        }
-
-        while (true) {
-            IndexAtual = perguntas.returnIndex();
-            proximaPergunta = perguntas.getNextQuestion();
-            System.out.println(proximaPergunta);
-
-            input.nextLine();
-            respostaJogo = input.nextLine().toUpperCase();
-
-            if (!respostaJogo.equals("A") && !respostaJogo.equals("B") && !respostaJogo.equals("C")) {
-                throw new EntradaInvalida("Alternativa invalida !");
-            }
-
-            //chama o map pra comparar a resposta da pergunta atual
-            respostaCorreta = respostas.getResposta(IndexAtual);
-
-            //debug
-            System.out.println(respostaCorreta);
-            
-            //confere se a resposta dada é igual a resposta certa
-            if (respostaJogo.equals(respostaCorreta)) {
-                System.out.println("Resposta Certa!");
-                //aumenta os acertos
-                voce.increaseAcertos();
-            } else {
-                System.out.println("Resposta Errada!");
-                voce.increaseErros();
-            }
-
-            //condição de parada
-            if (IndexAtual == perguntas.getTamanhoPerguntas() - 1) {
-                if (usuario.getAcertos() > usuario.getErros()) {
-                    System.out.println("MATCH !");
-                    break;
-                } else {
-                    System.out.println("Hoje não foi seu dia... mais sorte da proxima vez!");
-                    break;
+                System.out.println("Escolha qual perfil para ver a compatibilidade :");
+                System.out.println("1 - Padrão");
+                System.out.println("2 - Perfil do Usuario");
+                opcaoPerfil = input.nextInt();
+                if (opcaoPerfil == 1) {
+                    respostas.lerRespostasArquivo("respostas_padrao.txt");
+                } else if (opcaoPerfil == 2) {
+                    respostas.lerRespostasArquivo("Perfil_usuario.txt");
                 }
-            }
-        }
+
+                while (true) {
+                    IndexAtual = perguntas.returnIndex();
+                    proximaPergunta = perguntas.getNextQuestion();
+                    System.out.println(proximaPergunta);
+
+                    input.nextLine();
+                    respostaJogo = input.nextLine().toUpperCase();
+
+                    if (!respostaJogo.equals("A") && !respostaJogo.equals("B") && !respostaJogo.equals("C")) {
+                        throw new EntradaInvalida("Alternativa invalida !");
+
+                    } 
+                //chama o map pra comparar a resposta da pergunta atual
+                respostaCorreta = respostas.getResposta(IndexAtual);
+                //debug
+                System.out.println(respostaCorreta);
+                // 
+                //confere se a resposta dada é igual a resposta certa
+                if(respostaJogo.equals(respostaCorreta)){
+                    System.out.println("Resposta Certa!");
+                    //aumenta os acertos
+                    voce.increaseAcertos();
+                } else {
+                    System.out.println("Resposta Errada!");
+                    voce.increaseErros();
+                }   
+                
+                //condição de parada
+                if(IndexAtual == perguntas.getTamanhoPerguntas() - 1){
+                    if(usuario.getAcertos() > usuario.getErros()){
+                        System.out.println("MATCH !");
+                        break;
+                    } else {
+                        System.out.println("Hoje não foi seu dia... mais sorte da proxima vez!");
+                        break;
+                    } 
+                }
+                //passa para a proxima pergunta
+                
+                
+
+                    }
+
+                    respostaCorreta = respostas.getResposta(IndexAtual);
+
+                    if (respostaJogo.equals(respostaCorreta)) {
+                        System.out.println("Resposta Certa!");
+                        voce.increaseAcertos();
+                    } else {
+                        System.out.println("Resposta Errada!");
+                        voce.increaseErros();
+                    }
+
+                    if (IndexAtual == perguntas.getTamanhoPerguntas()) {
+                        if (usuario.getAcertos() > usuario.getErros()) {
+                            System.out.println("MATCH !");
+                            break;
+                        } else {
+                            System.out.println("Hoje não foi seu dia... mais sorte da proxima vez!");
+                            break;
+                        }
+                    }
+                
             } else if (opcaoMenu == 2) {
                 while (true) {
                     IndexAtual = perguntas.returnIndex();
--- src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VerificaArquivo.java
+++ src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VerificaArquivo.java
@@ -8,47 +8,6 @@ import java.util.Scanner;
 
 public class VerificaArquivo {
     public static void main(String[] args) {
-        try {
-            VerificarResposta respostas = new VerificarResposta(); 
-            QuestionarioController perguntas = new QuestionarioController();
-            int currentLineFile;
-
-            File arquivoUsuario = new File("Perfil_Usuario.txt");
-            if (arquivoUsuario.exists()) {
-                Scanner leitorArquivo = new Scanner(arquivoUsuario);
-                currentLineFile = 0;
-                while (leitorArquivo.hasNextLine()) {
-                    String respostasArquivo = leitorArquivo.nextLine();
-                    respostas.salvarRespostas(respostasArquivo, currentLineFile);
-                    currentLineFile++;
-                }
-                leitorArquivo.close();
-            } else {
-                File file = new File("respostas_padrao.txt");
-                if (file.exists()) {
-                    Scanner leitorArquivo = new Scanner(file);
-                    currentLineFile = 0;
-
-                    while (leitorArquivo.hasNextLine()) {
-                        String respostasArquivo = leitorArquivo.nextLine();
-                        respostas.salvarRespostas(respostasArquivo, currentLineFile);
-                        currentLineFile++;
-                    }
-                    leitorArquivo.close();
-                } else {
-                    file.createNewFile();
-                    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-                        for (int i = 0; i < perguntas.getTamanhoPerguntas(); i++) {
-                            writer.write("A");
-                            writer.newLine();
-                        }
-                    } catch (IOException e) {
-                        System.err.println("Erro ao escrever no arquivo: " + e.getMessage());
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
     }
 }
