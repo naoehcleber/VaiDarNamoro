4de99069787811aa0fb5285def66572658695590
Anabia <ana.00000850038@unicap.br>
2024-06-10 22:46:11 -0300

    reajustando na main

--- src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VaiDarNamoroTerminal.java
+++ src/main/java/com/mycompany/vaidarnamoroterminal/Controller/VaiDarNamoroTerminal.java
@@ -1,12 +1,12 @@
 package com.mycompany.vaidarnamoroterminal.Controller;
-
 import com.mycompany.vaidarnamoroterminal.Controller.EntradaInvalida;
-import com.mycompany.vaidarnamoroterminal.Controller.Usuario;
-import com.mycompany.vaidarnamoroterminal.Entity.Perguntas;
-import com.mycompany.vaidarnamoroterminal.Entity.Pessoa;
-import com.mycompany.vaidarnamoroterminal.Entity.Questionario;
-import com.mycompany.vaidarnamoroterminal.Model.ListaPerguntas;
-import com.mycompany.vaidarnamoroterminal.Model.Respostas;
+import com.mycompany.vaidarnamoroterminal.Controller.QuestionarioController;
+import com.mycompany.vaidarnamoroterminal.Controller.UsuarioController;
+import com.mycompany.vaidarnamoroterminal.Controller.VerificarResposta;
+import com.mycompany.vaidarnamoroterminal.Entity.Questao;
+import com.mycompany.vaidarnamoroterminal.Entity.Usuario;
+
+
 
 import java.util.Scanner;
 import java.io.File;
@@ -22,7 +22,17 @@ public class VaiDarNamoroTerminal {
 
     public static void main(String[] args) throws EntradaInvalida {
         Scanner input = new Scanner(System.in);
+        int opcaoPerfil;
+        
 
+        QuestionarioController perguntas = new QuestionarioController();
+        VerificarResposta respostas = new VerificarResposta();
+        UsuarioController voce = new UsuarioController();
+        Usuario usuario = new Usuario();
+        int IndexAtual;
+        Questao proximaPergunta;
+        String respostaJogo;
+        String respostaCorreta;
         //ListaPerguntas perguntas = new ListaPerguntas();
         //Respostas respostas = new Respostas();
         //Pessoa pessoa = new Pessoa();
@@ -30,13 +40,9 @@ public class VaiDarNamoroTerminal {
 
         int opcaoMenu;
         int currentLineFile;
-        int IndexAtual;
-        int opcaoPerfil;
+        
+        
         String nomeUsuario;
-        String respostaJogo;
-        String respostaCorreta;
-        Questionario proximaPergunta;
-
         
 
         do {
@@ -47,8 +53,8 @@ public class VaiDarNamoroTerminal {
                 throw new EntradaInvalida("Insira seu nome por favor");
             }
 
-            voce.setNome(nomeUsuario);
-            System.out.println("Seja bem vindo " + voce.getNome());
+            usuario.setNome(nomeUsuario);
+            System.out.println("Seja bem vindo " + usuario.getNome());
         } while (nomeUsuario.equals(""));
 
         while (true) {
@@ -62,7 +68,56 @@ public class VaiDarNamoroTerminal {
             }
 
             if (opcaoMenu == 1) {
-                
+                perguntas.restartCounter();
+        System.out.println("Escolha qual perfil para ver a compatibilidade :");
+        System.out.println("1 - Padrão");
+        System.out.println("2 - Perfil do Usuario");
+        opcaoPerfil = input.nextInt();
+        if (opcaoPerfil == 1) {
+            respostas.lerRespostasArquivo("respostas_padrao.txt");
+        } else if (opcaoPerfil == 2) {
+            respostas.lerRespostasArquivo("Perfil_usuario.txt");
+        }
+
+        while (true) {
+            IndexAtual = perguntas.returnIndex();
+            proximaPergunta = perguntas.getNextQuestion();
+            System.out.println(proximaPergunta);
+
+            input.nextLine();
+            respostaJogo = input.nextLine().toUpperCase();
+
+            if (!respostaJogo.equals("A") && !respostaJogo.equals("B") && !respostaJogo.equals("C")) {
+                throw new EntradaInvalida("Alternativa invalida !");
+            }
+
+            //chama o map pra comparar a resposta da pergunta atual
+            respostaCorreta = respostas.getResposta(IndexAtual);
+
+            //debug
+            System.out.println(respostaCorreta);
+            
+            //confere se a resposta dada é igual a resposta certa
+            if (respostaJogo.equals(respostaCorreta)) {
+                System.out.println("Resposta Certa!");
+                //aumenta os acertos
+                voce.increaseAcertos();
+            } else {
+                System.out.println("Resposta Errada!");
+                voce.increaseErros();
+            }
+
+            //condição de parada
+            if (IndexAtual == perguntas.getTamanhoPerguntas() - 1) {
+                if (usuario.getAcertos() > usuario.getErros()) {
+                    System.out.println("MATCH !");
+                    break;
+                } else {
+                    System.out.println("Hoje não foi seu dia... mais sorte da proxima vez!");
+                    break;
+                }
+            }
+        }
             } else if (opcaoMenu == 2) {
                 while (true) {
                     IndexAtual = perguntas.returnIndex();
